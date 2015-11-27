8d694be0b9b376bf7ea18502a1649ac9a0b87069
xinhuang <xinhuang.abc@gmail.com>
2015-11-27 17:49:31 +0800

    pip install ipython

--- script/install.py
+++ script/install.py
@@ -1,11 +1,14 @@
 import sys
 import subprocess
 import os
+import platform
 
 root_dir = sys.argv[1]
 package_file = os.path.join(root_dir, "etc", "packages.csv")
 download_dir = os.path.join(root_dir, "download/")
 bin_dir = os.path.join(root_dir, "bin/")
+
+system = platform.system()
 
 def download(url):
     print get_wget() + " --no-check-certificate -P {prefix} {url}".format(url=url, prefix=download_dir)
@@ -20,13 +23,19 @@ def install(cmd):
 def get_wget():
     return os.path.join(bin_dir, "wget.exe")
 
+def is_whitespace_or_none(s):
+    return s is None or len(s.strip()) == 0
+
 packages = [line.strip() for line in open(package_file, 'r')]
 
 for p in packages:
     if p.startswith('#'):
         continue
     name, url, cmd = p.split(',')
-    print "name: {0}, url: {1}, cmd: {2}".format(name, url, cmd)
+    # print "name: {0}, url: {1}, cmd: {2}".format(name, url, cmd)
     localpath = "{0}/{1}"
-    filepath = download(url)
-    install(cmd.format(path=filepath))
+    if (is_whitespace_or_none(url)):
+        install(cmd)
+    else:
+        filepath = download(url)
+        install(cmd.format(path=filepath))
