ecf2b23e56ade36364da8434ac23f205fdad7eae
xinhuang <xinhuang.abc@gmail.com>
2015-11-27 17:30:45 +0800

    Change install execution directory to download
    to keep intermediate downloaded file are in download folder

--- script/install.py
+++ script/install.py
@@ -2,16 +2,23 @@ import sys
 import subprocess
 import os
 
-package_file = sys.argv[1]
-download_dir = sys.argv[2]
+root_dir = sys.argv[1]
+package_file = os.path.join(root_dir, "etc", "packages.csv")
+download_dir = os.path.join(root_dir, "download/")
+bin_dir = os.path.join(root_dir, "bin/")
 
 def download(url):
-    print "bin/wget.exe --no-check-certificate -P download/ {url}".format(url=url)
-    subprocess.call(['bin/wget.exe', '--no-check-certificate', '-P', 'download/', url])
-    return 'download/' + os.path.basename(url)
+    print get_wget() + " --no-check-certificate -P {prefix} {url}".format(url=url, prefix=download_dir)
+    subprocess.call([get_wget(), '--no-check-certificate', '-P', 'download/', url])
+    return download_dir + os.path.basename(url)
 
 def install(cmd):
+    os.chdir(download_dir)
     subprocess.call(cmd)
+    os.chdir(root_dir)
+
+def get_wget():
+    return os.path.join(bin_dir, "wget.exe")
 
 packages = [line.strip() for line in open(package_file, 'r')]
 
